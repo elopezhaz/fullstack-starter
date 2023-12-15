df19882e83d757afb01b716559396425eecd8604
Erica <erica.lopezhaz@interclypse.com>
2023-12-15 08:36:15 -0500

    inventory duck style changes

--- ui/src/ducks/inventory/index.js
+++ ui/src/ducks/inventory/index.js
@@ -1,17 +1,61 @@
+import * as alerts from '../alerts'
+import axios from 'axios'
 import { createAction, handleActions } from 'redux-actions'
 
 const actions = {
   INVENTORY_GET_ALL: 'inventory/get_all',
+  INVENTORY_GET_ALL_PENDING: 'inventory/get_all_PENDING',
+  INVENTORY_SAVE: 'inventory/save',
+  INVENTORY_DELETE: 'inventory/delete',
+  INVENTORY_REFRESH: 'inventory/refresh',
 }
 
 export let defaultState = {
-  all: []
+  all: [],
+  fetched: false,
 }
 
-export const findInventory = createAction(actions.INVENTORY_GET_ALL, () => {
-  //TODO
-})
+export const findInventory = createAction(
+  actions.INVENTORY_GET_ALL,
+  () => (dispatch, getState, config) =>
+    axios
+      .get(`${config.restAPIUrl}/inventory`)
+      .then((suc) => {
+        dispatch(refreshInventory(suc.data))
+        alerts.openSuccess()
+      })
+)
+
+export const saveInventory = createAction(actions.INVENTORY_SAVE, (inventory) =>
+  (dispatch, getState, config) => axios
+    .post(`${config.restAPIUrl}/inventory`, inventory)
+    .then((suc) => {
+      const invs = []
+      getState().inventory.all.forEach(inv => {
+        if (inv.id !== suc.data.id) {
+          invs.push(inv)
+        }
+      })
+      invs.push(suc.data)
+      dispatch(refreshInventory(invs))
+      alerts.openSuccess()
+    })
+)
+
+export const refreshInventory = createAction(actions.INVENTORY_REFRESH, (payload) =>
+  (dispatcher, getState, config) =>
+    payload.sort((inventoryA, inventoryB) =>
+      inventoryA.name < inventoryB.name ? -1 : inventoryA.name > inventoryB.name ? 1 : 0)
+)
 
 export default handleActions({
-  //TODO
+  [actions.INVENTORY_GET_ALL_PENDING]: (state) => ({
+    ...state,
+    fetched: false
+  }),
+  [actions.INVENTORY_REFRESH]: (state, action) => ({
+    ...state,
+    all: action.payload,
+    fetched: true,
+  })
 }, defaultState)
